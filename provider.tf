1e3dcc50b2013b28b9a9f18a7e1ae89b5f4ad816
Anas Almohana <anas.almohana@dnb.no>
2023-04-26 13:35:34 +0200

    master sync

---
+++ provider.tf
@@ -0,0 +1,25 @@
+terraform {
+  cloud {
+    organization = "ntcdev"
+
+    workspaces {
+      name = "first"
+    }
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.34.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
